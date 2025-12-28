884df86765653d04036a6fd786ff80231971767e
Ardatatar48 <ardaatatar2003@gmail.com>
2025-12-29 00:03:18 +0200

    Update CarForm.java

--- database/src/CarForm.java
+++ database/src/CarForm.java
@@ -3,7 +3,7 @@ import javax.swing.table.DefaultTableModel; // data structure and updates for th
 import java.awt.*; // layouts, colors, fonts, and window events
 import java.sql.*; // jdbc
 
-// main window of the application
+// main window of the application isik deneme
 public class CarForm extends JFrame {
 
     private JTextField txtBrand;
